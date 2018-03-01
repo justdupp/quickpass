f0e6d37469a933bbc40c1d6d89a0521e6f7e8736
justdupp <hqxhj1990@163.com>
2018-03-01 22:21:20 +0800

    quickpass 基础服务搭建

---
+++ quickpass-service/src/main/java/hecc/pay/OrdersApplication.java
@@ -0,0 +1,16 @@
+package hecc.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther xuhoujun
+ * @Description:
+ * @Date: Created In 下午10:11 on 2018/3/1.
+ */
+@SpringBootApplication
+public class OrdersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersApplication.class, args);
+    }
+}
