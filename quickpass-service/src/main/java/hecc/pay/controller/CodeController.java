91ecdcdaa8f41cf41e48442967f7383f538801cc
justdupp <hqxhj1990@163.com>
2018-03-02 23:57:49 +0800

    添加码控制器（待实现）

---
+++ quickpass-service/src/main/java/hecc/pay/controller/CodeController.java
@@ -0,0 +1,19 @@
+package hecc.pay.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther xuhoujun
+ * @Description: 闪付 -- 码控制器
+ * @Date: Created In 下午11:55 on 2018/3/2.
+ */
+@RestController
+@RequestMapping("/api/code")
+public class CodeController extends BaseController {
+    private Logger logger = LoggerFactory.getLogger(CodeController.class);
+
+    // TODO: 2018/3/2 待添加 
+}
