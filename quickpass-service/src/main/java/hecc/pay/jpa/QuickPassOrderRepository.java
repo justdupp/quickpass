b44a8b5d7f04ea212b93fe9825a12087bb6d2f5c
justdupp <hqxhj1990@163.com>
2018-03-05 20:18:12 +0800

    修改jpa接口路径

--- quickpass-service/src/main/java/hecc/pay/jpa/QuickPassOrderRepository.java
+++ quickpass-service/src/main/java/hecc/pay/jpa/QuickPassOrderRepository.java
@@ -1,11 +1,12 @@
-package hecc.pay.entity;
+package hecc.pay.jpa;
 
+import hecc.pay.entity.QuickPassOrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @Auther xuhoujun
  * @Description: 订单jpa
- * @Date: Created In 下午5:00 on 2018/3/4.
+ * @Date: Created In 下午8:15 on 2018/3/5.
  */
 public interface QuickPassOrderRepository extends JpaRepository<QuickPassOrderEntity,Long> {
 }
