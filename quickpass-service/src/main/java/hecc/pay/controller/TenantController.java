56692980f0996b4f68cbb9bc84878160a3ddbaab
justdupp <hqxhj1990@163.com>
2018-03-18 14:45:30 +0800

    更新租户api

--- quickpass-service/src/main/java/hecc/pay/controller/TenantController.java
+++ quickpass-service/src/main/java/hecc/pay/controller/TenantController.java
@@ -5,8 +5,10 @@ import hecc.pay.client.tenant.TenantEntityVO;
 import hecc.pay.entity.QuickPassCreditCardEntity;
 import hecc.pay.entity.QuickPassTenantEntity;
 import hecc.pay.jpa.QuickPassCreditCardRepository;
+import hecc.pay.jpa.QuickPassTenantRepository;
 import hecc.pay.service.TenantService;
 import hecc.pay.vos.TenantInfoVO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,8 +31,12 @@ public class TenantController extends BaseController {
     @Autowired
     private QuickPassCreditCardRepository creditCardRepository;
 
+    @Autowired
+    private QuickPassTenantRepository tenantRepository;
+
+    @ApiOperation("获取租户信息")
     @RequestMapping(value = "/info", method = RequestMethod.GET)
-    public ResponseVO merchantInformation(@RequestHeader String platform, @RequestHeader Long tenantId) {
+    public ResponseVO tenantInfo(@RequestHeader String platform, @RequestHeader Long tenantId) {
         TenantEntityVO tenantEntity = tenantClient.getTenant(tenantId);
         QuickPassTenantEntity tenant = tenantService.getQuickPassTenantEntity(platform, tenantEntity);
         List<QuickPassCreditCardEntity> creditCardList = creditCardRepository.findByTenantTenantIdAndDelIsFalse(tenantId);
@@ -45,5 +51,12 @@ public class TenantController extends BaseController {
             parentUser = tenantClient.getTenant(tenant.code.tenant.tenantId);
         }
         return successed(new TenantInfoVO(parentUser, tenantEntity, tenant, creditCardList));
+    }
+
+    @ApiOperation("租户是否激活")
+    @RequestMapping(value = "/isOpen", method = RequestMethod.GET)
+    public ResponseVO isOpenTenant(@RequestHeader Long tenantId) {
+        QuickPassTenantEntity tenantEntity = tenantRepository.findOneByTenantIdAndDelIsFalse(tenantId);
+        return successed(tenantEntity.active);
     }
 }
