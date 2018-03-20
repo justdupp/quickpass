e4db5054364eca4dda20ddbfd48eceafd4a2576d
justdupp <hqxhj1990@163.com>
2018-03-20 21:19:11 +0800

    添加收益实体

---
+++ quickpass-service/src/main/java/hecc/pay/entity/QuickPassProfitEntity.java
@@ -0,0 +1,41 @@
+package hecc.pay.entity;
+
+import hecc.pay.controller.BaseController;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * @Auther xuhoujun
+ * @Description:
+ * @Date: Created In 下午9:14 on 2018/3/20.
+ */
+@Entity
+@Table(name = "profit")
+public class QuickPassProfitEntity extends BaseEntity {
+
+    /**
+     * 分润所属码
+     */
+    @ManyToOne
+    public QuickPassCodeEntity code;
+
+    /**
+     * 分润所属租户
+     */
+    @ManyToOne
+    public QuickPassTenantEntity tenant;
+
+    /**
+     * 原订单
+     */
+    @ManyToOne
+    public QuickPassOrderEntity order;
+
+    /**
+     * 收益（单位：分）
+     */
+    public Integer profit;
+
+}
---
+++ quickpass-service/src/main/java/hecc/pay/jpa/QuickPassProfitRepository.java
@@ -0,0 +1,12 @@
+package hecc.pay.jpa;
+
+import hecc.pay.entity.QuickPassProfitEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Auther xuhoujun
+ * @Description:
+ * @Date: Created In 下午9:16 on 2018/3/20.
+ */
+public interface QuickPassProfitRepository extends JpaRepository<QuickPassProfitEntity,Long> {
+}
