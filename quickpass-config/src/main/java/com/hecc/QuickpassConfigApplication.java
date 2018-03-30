b20698634bdcdf35563b900a119a57fc4c10220c
justdupp <hqxhj1990@163.com>
2018-03-30 21:23:07 +0800

    高可用配置中心

--- quickpass-config/src/main/java/com/hecc/QuickpassConfigApplication.java
+++ quickpass-config/src/main/java/com/hecc/QuickpassConfigApplication.java
@@ -3,9 +3,11 @@ package com.hecc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class QuickpassConfigApplication {
 
 	public static void main(String[] args) {
