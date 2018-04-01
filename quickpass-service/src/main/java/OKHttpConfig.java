a2105ba4a3fea1341797576b3de7abf0200702f3
justdupp <hqxhj1990@163.com>
2018-04-01 17:53:23 +0800

    添加httpconfig

---
+++ quickpass-service/src/main/java/OKHttpConfig.java
@@ -0,0 +1,26 @@
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Auther xuhoujun
+ * @Description: okHttp Bean
+ * @Date: Created In 下午5:44 on 2018/4/1.
+ */
+@Configuration
+public class OKHttpConfig {
+
+    private static final long OK_HTTP_TIME_OUT_SECOND = 60;
+
+    @Bean
+    public OkHttpClient createOkHttpClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(OK_HTTP_TIME_OUT_SECOND, TimeUnit.SECONDS)
+                .readTimeout(OK_HTTP_TIME_OUT_SECOND, TimeUnit.SECONDS)
+                .writeTimeout(OK_HTTP_TIME_OUT_SECOND, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true);
+        return builder.build();
+    }
+}
