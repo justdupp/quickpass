c1f1a687dc8b34da8dfef232b50529f817f3ab53
justdupp <hqxhj1990@163.com>
2018-04-08 22:31:21 +0800

    添加credit服务

---
+++ credit-service/src/main/java/com/credit/CreditServiceApplication.java
@@ -0,0 +1,12 @@
+package com.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditServiceApplication.class, args);
+	}
+}
---
+++ credit-service/src/test/java/com/credit/CreditServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.credit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
